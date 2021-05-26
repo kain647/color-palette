916f4007eab1e20e29132612c11036baf8492f68
Aleksey <aleksey.amelchenko@gmail.com>
2021-05-26 12:40:57 +0300

    Initialize project using Create React App

--- src/tabs/index.js
+++ src/tabs/index.js
@@ -1,5 +1,398 @@
 import React, { Component } from "react";
-import { Container, TabsBox, Tab, ContentBox, TitleContent } from "./styles";
+import {
+  Container,
+  TitleProject,
+  TabsBox,
+  Tab,
+  ContentBox,
+  TitleContent,
+  ColorContainer,
+  ColorBox
+} from "./styles";
+
+const tabs = [
+  {
+    label: "#f44336",
+    title: "Reds"
+  },
+  {
+    label: "#e91e63",
+    title: "Pinks"
+  },
+  {
+    label: "#9c27b0",
+    title: "Purples"
+  },
+  {
+    label: "#673ab7",
+    title: "Deep-Purples"
+  },
+  {
+    label: "#3f51b5",
+    title: "Indigos"
+  },
+  {
+    label: "#2196f3",
+    title: "Blues"
+  },
+  {
+    label: "#03a9f4",
+    title: "Light-Blues"
+  },
+  {
+    label: "#00bcd5",
+    title: "Cyans"
+  },
+  {
+    label: "#009688",
+    title: "Teals"
+  },
+  {
+    label: "#4caf50",
+    title: "Greens"
+  },
+  {
+    label: "#8bc34a",
+    title: "Light-Greens"
+  },
+  {
+    label: "#cddc39",
+    title: "Limes"
+  },
+  {
+    label: "#ffeb3b",
+    title: "Yellows"
+  },
+  {
+    label: "#ff9800",
+    title: "Ambers"
+  },
+  {
+    label: "#ff5722",
+    title: "Deep-Oranges"
+  },
+  {
+    label: "#795548",
+    title: "Browns"
+  },
+  {
+    label: "#9e9e9e",
+    title: "Greys"
+  },
+  {
+    label: "#607d8b",
+    title: "Blue-Greys"
+  }
+];
+
+const colors = {
+  reds: [
+    "#FF6659",
+    "#FF6154",
+    "#FF5C4F",
+    "#ff574A",
+    "#ff5245",
+    "#fe4d40",
+    "#f9483b",
+    "#f44336",
+    "#EF3E31",
+    "#EA392C",
+    "#E53427",
+    "#E02F22",
+    "#DB2A1D",
+    "#D62518",
+    "#D12013"
+  ],
+  pinks: [
+    "#FF4186",
+    "#FF3C81",
+    "#FF377C",
+    "#FD3277",
+    "#F82D72",
+    "#F3286D",
+    "#EE2368",
+    "#E91E63",
+    "#E4195E",
+    "#DF1459",
+    "#DA0F54",
+    "#D50A4F",
+    "#D0054A",
+    "#CB0045",
+    "#C60040"
+  ],
+  purples: [
+    "#bf4ad3",
+    "#ba45ce",
+    "#b540c9",
+    "#b03bc4",
+    "#ab36bf",
+    "#a631ba",
+    "#a12cb5",
+    "#9c27b0",
+    "#9722ab",
+    "#921da6",
+    "#8d18a1",
+    "#88139c",
+    "#830e97",
+    "#7e0992",
+    "#79048d"
+  ],
+  "deep-purples": [
+    "#8A5DDA",
+    "#8558D5",
+    "#8053D0",
+    "#7B4ECB",
+    "#7649C6",
+    "#7144C1",
+    "#6C3FBC",
+    "#673AB7",
+    "#6235B2",
+    "#5D30AD",
+    "#582BA8",
+    "#5326A3",
+    "#4E219E",
+    "#491C99",
+    "#441794"
+  ],
+  indigos: [
+    "#6274D8",
+    "#5D6FD3",
+    "#586ACE",
+    "#5365C9",
+    "#4E60C4",
+    "#495BBF",
+    "#4456BA",
+    "#3F51B5",
+    "#3A4CB0",
+    "#3547AB",
+    "#3042A6",
+    "#2B3DA1",
+    "#26389C",
+    "#213397",
+    "#1C2E92"
+  ],
+  blues: [
+    "#44B9FF",
+    "#3FB4FF",
+    "#3AAFFF",
+    "#35AAFF",
+    "#30A5FF",
+    "#2BA0FD",
+    "#269BF8",
+    "#2196F3",
+    "#1C91EE",
+    "#178CE9",
+    "#1287E4",
+    "#0D82DF",
+    "#087DDA",
+    "#0378D5",
+    "#0073D0"
+  ],
+  "light-blues": [
+    "#26CCFF",
+    "#21C7FF",
+    "#1CC2FF",
+    "#17BDFF",
+    "#12B8FF",
+    "#0DB3FE",
+    "#08AEF9",
+    "#03A9F4",
+    "#00A4EF",
+    "#009FEA",
+    "#009AE5",
+    "#0095E0",
+    "#0090DB",
+    "#008BD6",
+    "#0086D1"
+  ],
+  cyans: [
+    "#23DFF8",
+    "#1EDAF3",
+    "#19D5EE",
+    "#14D0E9",
+    "#0FCBE4",
+    "#0AC6DF",
+    "#05C1DA",
+    "#00BCD5",
+    "#00B7D0",
+    "#00B2CB",
+    "#00ADC6",
+    "#00A8C1",
+    "#00A3BC",
+    "#009EB7",
+    "#0099B2"
+  ],
+  teals: [
+    "#23B9AB",
+    "#1EB4A6",
+    "#19AFA1",
+    "#14AA9C",
+    "#0FA597",
+    "#0AA092",
+    "#059B8D",
+    "#009688",
+    "#009183",
+    "#008C7E",
+    "#008779",
+    "#008274",
+    "#007D6F",
+    "#00786A",
+    "#007365"
+  ],
+  greens: [
+    "#6FD273",
+    "#6ACD6E",
+    "#65C869",
+    "#60C364",
+    "#5BBE5F",
+    "#56B95A",
+    "#51B455",
+    "#4CAF50",
+    "#47AA4B",
+    "#42A546",
+    "#3DA041",
+    "#389B3C",
+    "#339637",
+    "#2E9132",
+    "#298C2D"
+  ],
+  "light-greens": [
+    "#AEE66D",
+    "#A9E168",
+    "#A4DC63",
+    "#9FD75E",
+    "#9AD259",
+    "#95CD54",
+    "#90C84F",
+    "#8BC34A",
+    "#86BE45",
+    "#81B940",
+    "#7CB43B",
+    "#77AF36",
+    "#72AA31",
+    "#6DA52C",
+    "#68A027"
+  ],
+  limes: [
+    "#F0FF5C",
+    "#EBFA57",
+    "#E6F552",
+    "#E1F04D",
+    "#DCEB48",
+    "#D7E643",
+    "#D2E13E",
+    "#CDDC39",
+    "#C8D734",
+    "#C3D22F",
+    "#BECD2A",
+    "#B9C825",
+    "#B4C320",
+    "#AFBE1B",
+    "#AAB916"
+  ],
+  yellows: [
+    "#FFFF5E",
+    "#FFFF59",
+    "#FFFF54",
+    "#FFFF4F",
+    "#FFFA4A",
+    "#FFF545",
+    "#FFF040",
+    "#FFEB3B",
+    "#FAE636",
+    "#F5E131",
+    "#F0DC2C",
+    "#EBD727",
+    "#E6D222",
+    "#E1CD1D",
+    "#DCC818"
+  ],
+  ambers: [
+    "#FFBB23",
+    "#FFB61E",
+    "#FFB119",
+    "#FFAC14",
+    "#FFA70F",
+    "#FFA20A",
+    "#FF9D05",
+    "#FF9800",
+    "#FA9300",
+    "#F58E00",
+    "#F08900",
+    "#EB8400",
+    "#E67F00",
+    "#E17A00",
+    "#DC7500"
+  ],
+  "deep-oranges": [
+    "#FF7A45",
+    "#FF7540",
+    "#FF703B",
+    "#FF6B36",
+    "#FF6631",
+    "#FF612C",
+    "#FF5C27",
+    "#FF5722",
+    "#FA521D",
+    "#F54D18",
+    "#F04813",
+    "#EB430E",
+    "#E63E09",
+    "#E13904",
+    "#DC3400"
+  ],
+  browns: [
+    "#9C786B",
+    "#977366",
+    "#926E61",
+    "#8D695C",
+    "#886457",
+    "#835F52",
+    "#7E5A4D",
+    "#795548",
+    "#745043",
+    "#6F4B3E",
+    "#6A4639",
+    "#654134",
+    "#603C2F",
+    "#5B372A",
+    "#563225"
+  ],
+  greys: [
+    "#C1C1C1",
+    "#BCBCBC",
+    "#B7B7B7",
+    "#B2B2B2",
+    "#ADADAD",
+    "#A8A8A8",
+    "#A3A3A3",
+    "#9E9E9E",
+    "#999999",
+    "#949494",
+    "#8F8F8F",
+    "#8A8A8A",
+    "#858585",
+    "#808080",
+    "#7B7B7B"
+  ],
+  "blue-greys": [
+    "#83A0AE",
+    "#7E9BA9",
+    "#7996A4",
+    "#74919F",
+    "#6F8C9A",
+    "#6A8795",
+    "#658290",
+    "#607D8B",
+    "#5B7886",
+    "#567381",
+    "#516E7C",
+    "#4C6977",
+    "#476472",
+    "#425F6D",
+    "#3D5A68"
+  ]
+};
 
 class Tabs extends Component {
   constructor(props) {
@@ -15,8 +408,10 @@ class Tabs extends Component {
   render() {
     const { active } = this.state;
     const { items } = this.props;
+    const colorSet = colors[tabs[active].title.toLowerCase()];
     return (
       <Container>
+        <TitleProject>COLOR PALETTE</TitleProject>
         <TabsBox>
           {items.map((item, index) => (
             <Tab
@@ -25,165 +420,32 @@ class Tabs extends Component {
                 this.changeTab(index);
               }}
               active={active === index}
-            >
-              {item.label}
-            </Tab>
+              label={item.label}
+            />
           ))}
         </TabsBox>
-        <ContentBox>{items[active].content}</ContentBox>
+        <ContentBox>
+          <TitleContent>{items[active].title}</TitleContent>
+          <ColorContainer>
+            {colorSet.map(item => {
+              return (
+                <ColorBox
+                  style={{
+                    background: item
+                  }}
+                >
+                  {item}
+                </ColorBox>
+              );
+            })}
+          </ColorContainer>
+        </ContentBox>
       </Container>
     );
   }
 }
 const Content = () => {
-  return (
-    <Tabs
-      items={[
-        {
-          label: "Tesla",
-          content: (
-            <p>
-              <TitleContent>Nikola Tesla</TitleContent> Serbian-American
-              inventor, electrical engineer, mechanical engineer, physicist, and
-              futurist best known for his contributions to the design of the
-              modern alternating current (AC) electrical supply system. Tesla
-              started working in the telephony and electrical fields before
-              emigrating to the United States in 1884 to work for Thomas Edison.
-              He soon struck out on his own with financial backers, setting up
-              laboratories/companies to develop a range of electrical devices.
-              His patented AC induction motor and transformer were licensed by
-              George Westinghouse, who also hired Tesla as a consultant to help
-              develop an alternating current system. Tesla is also known for his
-              high-voltage, high-frequency power experiments in New York and
-              Colorado Springs which included patented devices and theoretical
-              work used in the invention of radio communication, for his X-ray
-              experiments, and for his ill-fated attempt at intercontinental
-              wireless transmission in his unfinished Wardenclyffe Tower
-              project.
-              <p className="textRight">
-                <em>
-                  Find out more about Nikola Tesla from{" "}
-                  <a
-                    href="http://en.wikipedia.org/wiki/Nikola_Tesla"
-                    target="_blank"
-                  >
-                    Wikipedia
-                  </a>
-                  .
-                </em>
-              </p>
-            </p>
-          )
-        },
-        {
-          label: "da Vinci",
-          content: (
-            <p>
-              <TitleContent>Leonardo da Vinci</TitleContent> Italian Renaissance
-              polymath: painter, sculptor, architect, musician, mathematician,
-              engineer, inventor, anatomist, geologist, cartographer, botanist,
-              and writer. His genius, perhaps more than that of any other
-              figure, epitomized the Renaissance humanist ideal. Leonardo has
-              often been described as the archetype of the Renaissance Man, a
-              man of "unquenchable curiosity" and "feverishly inventive
-              imagination". He is widely considered to be one of the greatest
-              painters of all time and perhaps the most diversely talented
-              person ever to have lived. According to art historian Helen
-              Gardner, the scope and depth of his interests were without
-              precedent and "his mind and personality seem to us superhuman, the
-              man himself mysterious and remote". Marco Rosci states that while
-              there is much speculation about Leonardo, his vision of the world
-              is essentially logical rather than mysterious, and that the
-              empirical methods he employed were unusual for his time.
-              <p className="textRight">
-                <em>
-                  Find out more about Leonardo da Vinci from{" "}
-                  <a
-                    href="http://en.wikipedia.org/wiki/Leonardo_da_Vinci"
-                    target="_blank"
-                  >
-                    Wikipedia
-                  </a>
-                  .
-                </em>
-              </p>
-            </p>
-          )
-        },
-        {
-          label: "Einstein",
-          content: (
-            <p>
-              <TitleContent>Albert Einstein</TitleContent> German-born
-              theoretical physicist who developed the general theory of
-              relativity, one of the two pillars of modern physics (alongside
-              quantum mechanics). While best known for his mass–energy
-              equivalence formula E = mc2 (which has been dubbed "the world's
-              most famous equation"), he received the 1921 Nobel Prize in
-              Physics "for his services to theoretical physics, and especially
-              for his discovery of the law of the photoelectric effect". The
-              latter was pivotal in establishing quantum theory. Near the
-              beginning of his career, Einstein thought that Newtonian mechanics
-              was no longer enough to reconcile the laws of classical mechanics
-              with the laws of the electromagnetic field. This led to the
-              development of his special theory of relativity. He realized,
-              however, that the principle of relativity could also be extended
-              to gravitational fields, and with his subsequent theory of
-              gravitation in 1916, he published a paper on the general theory of
-              relativity.
-              <p className="textRight">
-                <em>
-                  Find out more about Albert Einstein from{" "}
-                  <a
-                    href="http://en.wikipedia.org/wiki/Albert_Einstein"
-                    target="_blank"
-                  >
-                    Wikipedia
-                  </a>
-                  .
-                </em>
-              </p>
-            </p>
-          )
-        },
-        {
-          label: "Newton",
-          content: (
-            <p>
-              <TitleContent>Isaac Newton</TitleContent> English physicist and
-              mathematician who is widely regarded as one of the most
-              influential scientists of all time and as a key figure in the
-              scientific revolution. His book Philosophiæ Naturalis Principia
-              Mathematica ("Mathematical Principles of Natural Philosophy"),
-              first published in 1687, laid the foundations for most of
-              classical mechanics. Newton also made seminal contributions to
-              optics and shares credit with Gottfried Leibniz for the invention
-              of the infinitesimal calculus. Newton's Principia formulated the
-              laws of motion and universal gravitation that dominated
-              scientists' view of the physical universe for the next three
-              centuries. It also demonstrated that the motion of objects on the
-              Earth and that of celestial bodies could be described by the same
-              principles. By deriving Kepler's laws of planetary motion from his
-              mathematical description of gravity, Newton removed the last
-              doubts about the validity of the heliocentric model of the cosmos.
-              <p className="textRight">
-                <em>
-                  Find out more about Isaac Newton from{" "}
-                  <a
-                    href="http://en.wikipedia.org/wiki/Isaac_Newton"
-                    target="_blank"
-                  >
-                    Wikipedia
-                  </a>
-                  .
-                </em>
-              </p>
-            </p>
-          )
-        }
-      ]}
-    />
-  );
+  return <Tabs items={tabs} />;
 };
 
 export default Content;
